43fb57c5fe182ca60fee7a0fd228135d97e7dc2e
Akshay Chordiya <akshaychordiya2@gmail.com>
2017-09-10 16:13:00 +0530

    Added adapter code to show news list

--- app/src/main/java/com/akshay/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/akshay/newsapp/ui/MainActivity.kt
@@ -2,7 +2,11 @@ package com.akshay.newsapp.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import com.akshay.newsapp.R
+import com.akshay.newsapp.adapter.NewsArticlesAdapter
+import com.akshay.newsapp.utils.toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * The Launcher Activity.
@@ -15,5 +19,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Setting up RecyclerView and adapter
+        val adapter = NewsArticlesAdapter {
+            toast("Clicked on item")
+        }
+        news_list.adapter = adapter
+        news_list.layoutManager = LinearLayoutManager(this)
     }
 }
