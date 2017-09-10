cc40f7644e9eaf65c918ac72ac05c64ce925bb7f
Akshay Chordiya <akshaychordiya2@gmail.com>
2017-09-10 16:14:45 +0530

    Final commit to fetch news from web and show on the UI

--- app/src/main/java/com/akshay/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/akshay/newsapp/ui/MainActivity.kt
@@ -5,8 +5,14 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import com.akshay.newsapp.R
 import com.akshay.newsapp.adapter.NewsArticlesAdapter
+import com.akshay.newsapp.api.NewsArticleService
+import com.akshay.newsapp.api.RetrofitHelper
+import com.akshay.newsapp.model.NewsSource
 import com.akshay.newsapp.utils.toast
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 /**
  * The Launcher Activity.
@@ -26,5 +32,21 @@ class MainActivity : AppCompatActivity() {
         }
         news_list.adapter = adapter
         news_list.layoutManager = LinearLayoutManager(this)
+
+        val newsArticleService = RetrofitHelper.retrofit.create(NewsArticleService::class.java)
+        val newsArticles = newsArticleService.getAllNewsArticles()
+        newsArticles.enqueue(object : Callback<NewsSource> {
+            override fun onResponse(call: Call<NewsSource>?, response: Response<NewsSource>?) {
+                response?.body()?.let { newsSource ->
+                    // Update the UI with the data
+                    adapter.swapItems(newsSource.articles)
+                    adapter.notifyDataSetChanged()
+                }
+            }
+
+            override fun onFailure(call: Call<NewsSource>?, t: Throwable?) {
+                toast("Unable to fetch the news")
+            }
+        })
     }
 }
